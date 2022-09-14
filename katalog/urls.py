e0dab262d98d6de7f3025cf294e40d21b7301af7
inaya.rahmanisa <inaya.rahmanisa@gmail.com>
2022-09-14 07:06:01 +0700

    made changes on katalog.urls

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,9 +1,9 @@
 # TODO: Implement Routings Here
 from django.urls import path, include
-from katalog.views import index
+from katalog.views import show_katalog
 
 app_name = 'katalog'
 
 urlpatterns = [
-    path('', index, name='show_katalog'),
+    path('', show_katalog, name='show_katalog'),
 ]
