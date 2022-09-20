81c6db7f415e094120491c0c63a9d91bd9e0bd4c
inaya.rahmanisa <inaya.rahmanisa@gmail.com>
2022-09-20 10:04:53 +0700

    adding '/' path

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
-
+from .models import WatchlistItem
 # Create your tests here.
+
+
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'mywatchlist'
 
 urlpatterns = [
+     path('', views.show_mywatchlist_html, name='show_mywatchlist_html'),
     path('html/', views.show_mywatchlist_html, name='show_mywatchlist_html'),
     path('json/', views.show_watchlist_json, name='show_watchlist_json'),
     path('xml/', views.show_watchlist_xml, name='show_watchlist_xml'),
